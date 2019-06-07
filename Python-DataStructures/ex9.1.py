098b196cc95c1c95817640de9fc538dd433e29ff
ktheylin <kheylin@yahoo.com>
2019-06-07 10:04:03 -0700

    added line count and print

--- Python-DataStructures/ex9.1.py
+++ Python-DataStructures/ex9.1.py
@@ -3,7 +3,10 @@ if len(fname) == 0:
 	fname = 'words.txt'
 fhand = open(fname)
 words = dict()
+line_no = 0
 for line in fhand:
+	line_no = line_no + 1
+	print("This is line number", line_no)
 	pieces = line.split()
 	for word in pieces:
 		if word not in words:
